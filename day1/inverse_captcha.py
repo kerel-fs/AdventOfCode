9178ac9577623b70c87a02ff2ad0e41bfcfcdc5c
Fabian P. Schmidt <kerel-fs@gmx.de>
2017-12-01 21:39:22 +0100

    day1: Add solution for part2: Inverse Captcha

--- day1/inverse_captcha.py
+++ day1/inverse_captcha.py
@@ -9,16 +9,25 @@ def get_sequence():
         sequence_str = lines[0].strip()
     return sequence_str
 
-def get_solution(sequence_str):
+def get_sum_of_equal_consecutive_pairs(sequence_str, shift):
     sequence = np.array(list(sequence_str), dtype=int)
     sum = 0
-    for current, next in zip(sequence, np.roll(sequence, -1)):
+    for current, next in zip(sequence, np.roll(sequence, shift)):
         if current == next:
             sum += current
     return sum
 
+def get_solution_part1(sequence_str):
+    return get_sum_of_equal_consecutive_pairs(sequence_str, -1)
+
+def get_solution_part2(sequence_str):
+    shift = int(0.5*len(sequence_str))
+    return get_sum_of_equal_consecutive_pairs(sequence_str, shift)
+
 
 if __name__=="__main__":
     s = get_sequence()
-    solution = get_solution(s)
-    print("Solution: {}".format(solution))
+    solution_part1 = get_solution_part1(s)
+    solution_part2 = get_solution_part2(s)
+    print("Solution part1: {}".format(solution_part1))
+    print("Solution part2: {}".format(solution_part2))
--- day1/tests.py
+++ day1/tests.py
@@ -1,21 +1,38 @@
 #!/usr/bin/env python3
 
 import unittest
-from inverse_captcha import get_solution
+from inverse_captcha import get_solution_part1, get_solution_part2
 
 
-class TestInverseCaptchaMethod(unittest.TestCase):
+class TestInverseCaptchaPart1Method(unittest.TestCase):
     def test1(self):
-        self.assertEqual(get_solution("1122"), 3)
+        self.assertEqual(get_solution_part1("1122"), 3)
 
     def test2(self):
-        self.assertEqual(get_solution("1111"), 4)
+        self.assertEqual(get_solution_part1("1111"), 4)
 
     def test3(self):
-        self.assertEqual(get_solution("1234"), 0)
+        self.assertEqual(get_solution_part1("1234"), 0)
 
     def test4(self):
-        self.assertEqual(get_solution("91212129"), 9)
+        self.assertEqual(get_solution_part1("91212129"), 9)
+
+
+class TestInverseCaptchaPart2Method(unittest.TestCase):
+    def test1(self):
+        self.assertEqual(get_solution_part2("1212"), 6)
+
+    def test2(self):
+        self.assertEqual(get_solution_part2("1221"), 0)
+
+    def test3(self):
+        self.assertEqual(get_solution_part2("123425"), 4)
+
+    def test4(self):
+        self.assertEqual(get_solution_part2("123123"), 12)
+
+    def test5(self):
+        self.assertEqual(get_solution_part2("12131415"), 4)
 
 
 if __name__ == "__main__":
